fb5e582efef0c56caf5704d33da394b37af7d5ab
IvanaCandyAguilar <ivanacandy1@gmail.com>
2021-11-08 00:37:35 +0100

    Lectura de precios

--- scrapyFarma.py
+++ scrapyFarma.py
@@ -8,10 +8,10 @@ soup = BeautifulSoup(page.content,'html.parser')
 
 #IMAGEN
 #PRECIO
-#PRODUCTO-MEDICAMENTO
 #CATEGORIA
 
 #MEDICAMENTO
+
 medicamento=soup.find_all('h2', class_="productitem--title")
 
 lista_medicamentos = list()
@@ -20,23 +20,40 @@ count=0
 
 for i in medicamento:
     if count<20:
-        med= i.text
-        med= med.replace('\n','')
-        med =med.replace('  ','')
-        lista_medicamentos.append(med)
+            med= i.text
+            med= med.replace('\n','')
+            med =med.replace('  ','')
+            lista_medicamentos.append(med)
 
     else:
         break
-    count +=1
+count +=1
 
 print(lista_medicamentos)
 
+#PRECIO
 
+precio=soup.find_all('span', class_="money")
 
+lista_precios = list()
 
+countj=0
 
+for j in precio:
+    if countj<20:
+            precio= j.text
+            precio= precio.replace('\n','')
+            precio =precio.replace('  ','')
+            precio = precio.replace(',','.')
+            lista_precios.append(precio)
+    else:
+        break
+countj +=1
+print(precio)
 
-#df=pd.DataFrame({'Nombre':lista_equipos,'Puntos':lista_equipos},index=list(range(1,21)))
+print(lista_precios)
+
+#df=pd.DataFrame({'Medicamento':lista_medicamentos,'Precio':lista_precios},index=list(range(1,20)))
 
 #print(df)
 
